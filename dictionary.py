bd67e58e6472039b47ad83d60355bea33df5a19a
eashtmodi <66251858+eashtmodi@users.noreply.github.com>
2020-09-19 21:07:03 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,25 @@
+name=input("WELCOME, PLEASE ENTER YOUR NAME: ")
+welcome="HEY, {} WELCOME TO YOUR FIRST E-DICTIONARY".format(name)
+print(welcome)
+print(name,"sorry for inconvenience but we are working on more words ")
+
+dictionaryWords={
+    "consider":"deem to be",
+    "minute":"infinitely or immeasurably small",
+    "accord":"concurrence of opinion",
+    "evident":"clearly revealed to the mind or the senses or judgment",
+    "practice":"a customary way of operation or behavior",
+    "intend":"have in mind as a purpose",
+    "concern":"something that interests you because it is important",
+    "commit":"perform an act, usually with a negative connotation",
+    "issue":"some situation or event that is thought about",
+    "approach":"move towards"
+}
+print("till then try key terms: ")
+print(dictionaryWords.keys())
+Word=input("Please type the word for which u want meaning: ")
+print(name, "where you go:" )
+print(dictionaryWords[Word])
+print("Thanks for taking Modi's service")
+print("May you come again!")
+input("Enter to exit")
